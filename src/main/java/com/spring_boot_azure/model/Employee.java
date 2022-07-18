b9a244ea7ac1091607f5691254a6140276ccde11
Sonu Saini <sonusainicse@gmail.com>
2022-07-18 23:36:19 +0530

    Entity class is updated

--- src/main/java/com/spring_boot_azure/model/Employee.java
+++ src/main/java/com/spring_boot_azure/model/Employee.java
@@ -24,6 +24,27 @@ public class Employee {
 		
 		@Column(name="email")
 		private String email;
+		
+		@Column(name="salary")
+		private long salary;
+		
+		public long getSalary() {
+			return salary;
+		}
+
+		public void setSalary(long salary) {
+			this.salary = salary;
+		}
+
+		public String getDepartment() {
+			return department;
+		}
+
+		public void setDepartment(String department) {
+			this.department = department;
+		}
+		@Column(name="department")
+		private String department;
 
 	public void setId(long id) {
 		this.id = id;
